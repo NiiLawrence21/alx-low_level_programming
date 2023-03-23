f7cb29ed2c17fdb16e9318622c72c487a10d0e77
NiiLawrence <adomlawrence21@gmail.com>
2023-03-23 15:53:09 -0700

    0x10

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,22 +1,36 @@
 #include "variadic_functions.h"
+	#include <stdio.h>
+	#include <stdarg.h>
+
+
 /**
- * print_numbers - a function that prints numbers, followed by a new line.
- * @separator: string to be printed between numbers
- * @n: number of integers passed to the function
+ * print_numbers - Prints numbers, followed by a new line.
+ * @separator: The string to be printed between numbers.
+ * @n: The number of integers passed to the function.
+ * @...: A variable number of numbers to be printed.
  */
-void print_numbers(const char *separator, const unsigned int n, ...)
+	void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list nums;
-	unsigned int i;
+	unsigned int index;
+
 
 	va_start(nums, n);
-	for (i = 0; i < n; i++)
+
+
+	for (index = 0; index < n; index++)
 	{
 		printf("%d", va_arg(nums, int));
-		if (separator != NULL && i < n - 1)
-			printf("%s", separator);
+
+
+		if (index != (n - 1) && separator != NULL)
+		printf("%s", separator);
 	}
 
+
+	printf("\n");
+
+
 	va_end(nums);
-	putchar('\n');
 }
+
--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,38 +1,46 @@
 #include "variadic_functions.h"
+	#include <stdio.h>
+	#include <stdarg.h>
+
+
 /**
- * print_strings - a function that prints strings, followed by a new line.
- * @separator: string to be printed between the strings
- * @n: number of strings passed to the function
+ * print_strings - Prints strings, followed by a new line.
+ * @separator: The string to be printed between strings.
+ * @n: The number of strings passed to the function.
+ * @...: A variable number of strings to be printed.
  *
- * If separator is NULL, don’t print it
- * If one of the string is NULL, print (nil) instead
- * Print a new line at the end of your function
+ * Description: If separator is NULL, it is not printed.
+ *              If one of the strings if NULL, (nil) is printed instead.
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-	va_list str;
-	unsigned int i;
-	char *s;
+	va_list strings;
+	char *str;
+	unsigned int index;
 
-	va_start(str, n);
-	for (i = 0; i < n; i++)
+
+	va_start(strings, n);
+
+
+	for (index = 0; index < n; index++)
 	{
-		s = va_arg(str, char *);
-		if (s != NULL)
-		{
-			printf("%s", s);
-		}
-		else
-		{
-			printf("(nil)");
-		}
+		str = va_arg(strings, char *);
 
-		if (separator != NULL && i != n - 1)
-		{
+
+		if (str == NULL)
+			printf("(nil)");
+			else
+			printf("%s", str);
+
+
+		if (index != (n - 1) && separator != NULL)
 			printf("%s", separator);
-		}
 	}
 
-	va_end(str);
-	putchar('\n');
+
+	printf("\n");
+
+
+	va_end(strings);
 }
+
